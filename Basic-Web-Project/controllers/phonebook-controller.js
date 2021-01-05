24934b3d78327100183cb3439753d89a4edb9575
NikolaiNikolaev <niki_nik_74@abv.bg>
2021-01-05 18:10:48 +0200

    Added email

--- Basic-Web-Project/controllers/phonebook-controller.js
+++ Basic-Web-Project/controllers/phonebook-controller.js
@@ -2,10 +2,16 @@ const phonebook = require('../phonebook');
 
 module.exports = {
   index: (req, res) => {
-    res.render('index');
+    let contacts = phonebook.getPhonebook
+    res.render('index', { contacts });
     // TODO: load index page
   },
   addPhonebookPost:(req, res) => {
     // TODO: add a phonebook object to the array
+    let name = req.body.name;
+    let number = req.body.number;
+    phonebook.addContact(name, number);
+
+    res.redirect('/');
   }
 }
